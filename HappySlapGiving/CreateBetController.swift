8ec6d20ea4912f2ce240610a7d5a83229cdbf146
pradawa <rpby6@mail.missouri.edu>
2019-11-18 21:28:01 -0600

    add winner attribute(textfield)

--- HappySlapGiving/CreateBetController.swift
+++ HappySlapGiving/CreateBetController.swift
@@ -16,12 +16,14 @@ class CreateBetController: UIViewController, UITextFieldDelegate, UITextViewDele
     @IBOutlet weak var username2: UITextField!
     @IBOutlet weak var incident: UITextView!
     
+    @IBOutlet weak var winner: UITextField!
+    
     @IBOutlet weak var placeHolderLabel: UILabel!
     @IBOutlet weak var winnerlabel: UILabel!
     
     @IBOutlet weak var confirmbutton: UIButton!
 
-    var done = false
+    var done = true
     
     // use to check if there is username2
     var valid = false
@@ -33,11 +35,13 @@ class CreateBetController: UIViewController, UITextFieldDelegate, UITextViewDele
         print("isUnfinishedBet")
         done = false
         winnerlabel.isHidden = true
+        winner.isHidden = true
     }
     @IBAction func isFinishedBet(_ sender: UIBarButtonItem) {
         print("isFinishedBet")
         done = true
         winnerlabel.isHidden = false
+        winner.isHidden = false
     }
     
     override func viewDidLoad() {
@@ -47,14 +51,15 @@ class CreateBetController: UIViewController, UITextFieldDelegate, UITextViewDele
         
         incident.delegate = self
         
-        winnerlabel.isHidden = true
+        winnerlabel.isHidden = false
+        winner.isHidden = false
         
         setConfirmButton(enabled: false)
         
         //listener, monitor the change of textfield
         username1.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
         username2.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
-        
+       winner.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
     }
     
     
@@ -63,6 +68,7 @@ class CreateBetController: UIViewController, UITextFieldDelegate, UITextViewDele
     @objc func textFieldChanged(_ textField:UITextField) {
         let user1 = username1.text
         let user2 = username2.text
+     //   let winner1 = winner.text
         let formFilled = user1 != nil && user1 != "" && user2 != nil && user2 != ""
         setConfirmButton(enabled: formFilled)
     }
@@ -142,6 +148,22 @@ class CreateBetController: UIViewController, UITextFieldDelegate, UITextViewDele
             }
             
         }, withCancel: nil)
+        
+        
+        // alarm for invalid winner input and username1 input
+        
+        if( username1.text != UserService.currentUserProfile?.username){
+            let alert = UIAlertController(title: "Warning", message: "Can you type your name correctly?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Back", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+        if (done == true && winner.text != username1.text && winner.text != username2.text){
+            let alert = UIAlertController(title: "Warning", message: "Please enter the correct winner username ", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Back", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
     }
     
     
@@ -162,6 +184,7 @@ class CreateBetController: UIViewController, UITextFieldDelegate, UITextViewDele
                 "username1": username1.text!,
                 "username2": username2.text!,
                 "incident": incident.text,
+                "winner": winner.text!,
                 
                 "timestamp": [".sv":"timestamp"]
             ]
@@ -208,6 +231,7 @@ class CreateBetController: UIViewController, UITextFieldDelegate, UITextViewDele
                     "username1": username1.text!,
                     "username2": username2.text!,
                     "incident": incident.text,
+                    "winner": winner.text!,
                     
                     "timestamp": [".sv":"timestamp"]
                 ]
